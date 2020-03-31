29a3528931255e872d8b21398ddf13780abbe197
Aaron Kessler <kessler.aaron@gmail.com>
2020-03-30 20:33:40 -0400

    joined governors list with main twitter data

--- 01_analysis.R
+++ 01_analysis.R
@@ -9,6 +9,7 @@ library(janitor)
 library(lubridate)
 library(tidytext)
 
+
 #pull from live site:
 raw <- read_csv("https://storage.googleapis.com/bln_prod/project/850c9bfc-7c02-4d48-b631-898a81ff4144/governors_20200328.csv?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=bln-storage%40big-local-news-267923.iam.gserviceaccount.com%2F20200330%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20200330T211143Z&X-Goog-Expires=86400&X-Goog-SignedHeaders=host&X-Goog-Signature=32f102ae691018635cdcee442b2b6aee8cc7e1fe73f3ea00fbf7dd96ee23c3fc807f1622fbd38f1ffa047474a521bd80938f7118333e891f957b5cbe4acc045287ad3b30ab0a85816225972912010fdf6fb18760ae6f017ba6b53de342c31420eafc56c66290dfac748f75e367b3e7e258b083ab85a8000a856765831f064292703db47d742ad876bf5affabb8f164dedded32197e303728b65525c8bc6464272bc4dadfe344c9795cdf2b48de75d58cfa49834f1fceadc15cb104dc99dee5049784baab37e8cff7a7bc5ae832ce31244d5f404c3698a864569d017e6f02a08ced9680f1f7be57dd0eeefc0bc04957e561d4f4a91487adeba80aa2a3f11e16d1",
                  col_types = cols(.default = "c"))
@@ -36,11 +37,7 @@ twdata_all <- twdata_all %>%
     speaker = paste0(governor, " (", abbreviation, "-", party_abbrev, ")")
   )
 
-#missing parties?
-twdata_all %>% 
-  filter(is.na(party_abbrev)) %>% 
-  count(state) %>% 
-  View()
+
 
 twdata_all %>% 
   filter(state == "Ohio") %>% 
@@ -50,9 +47,15 @@ twdata_all %>%
 # Will turn to:
 # https://ballotpedia.org/Partisan_composition_of_governors
 
-# Import processed governors table from saved Gsheet:
+# Import processed governors table from a saved Gsheet:
 governorslist <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR612UAkJ68pjn6Wh-ndohpUiugtT1SyTkqw8PBegzwuphh9Efyp711frrmdD7nLOnxnw778iTY3mpw/pub?gid=0&single=true&output=csv")
 
+#join
+joined <- left_join(twdata_all, governorslist, by = c("abbreviation" = "state_abbrev"))
+
+joined %>% 
+  filter(is.na(party_affiliation.x)) %>% 
+  View()
 
 
 #some quick gropuings explore
